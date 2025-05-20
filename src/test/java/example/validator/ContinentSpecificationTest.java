33158e7ac8dd6888ea89297698d206a446e6386a
이진우 <leejinwoo1126@gmail.com>
2025-05-20 21:03:10 +0900

    refactor: 유효성 검사 테스트에 test data builder 패턴 적용

--- src/test/java/example/validator/ContinentSpecificationTest.java
+++ src/test/java/example/validator/ContinentSpecificationTest.java
@@ -1,8 +1,11 @@
 package example.validator;
 
+import static example.builder.ContinentBuilder.*;
 import static org.assertj.core.api.Assertions.*;
 
 import example.*;
+import example.builder.ContinentBuilder;
+import example.builder.PlanetBuilder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +17,16 @@ class ContinentSpecificationTest extends AbstractPlanetTest {
         Planet planet = createPlanet(new Continent("아시아"), new Continent("유럽"));
 
         ContinentSpecification specification = new ContinentSpecification(2);
+
+        assertThat(specification.isSatisfiedBy(planet)).isTrue();
+    }
+
+    @DisplayName("Test Data Builder 패턴을 이용해 리팩터링하면 테스트와 관련 없는 불필요한 정보를 제거할 수 있다")
+    @Test
+    void continentSizeByTestDataBuilderPattern() {
+        Planet planet = PlanetBuilder.aPlanet().with(aContinent(), aContinent(), aContinent()).build();
+
+        ContinentSpecification specification = new ContinentSpecification(3);
 
         assertThat(specification.isSatisfiedBy(planet)).isTrue();
     }
--- src/test/java/example/validator/PriceSpecificationTest.java
+++ src/test/java/example/validator/PriceSpecificationTest.java
@@ -1,5 +1,11 @@
 package example.validator;
 
+import static example.Money.*;
+import static example.builder.AtomsphereBuilder.anAtomsphere;
+import static example.builder.ContinentBuilder.aContinent;
+import static example.builder.NationBuilder.*;
+import static example.builder.OceanBuilder.*;
+import static example.builder.PlanetBuilder.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import example.*;
@@ -8,15 +14,29 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PriceSpecificationTest extends AbstractPlanetTest {
-    @DisplayName("행성의 실제 제조 가격이 생쥐들이 제시한 계약 금액보다 작다")
+    @DisplayName("행성의 전체 제조 가격이 생쥐들이 제시한 계약 금액보다 작다")
     @Test
     void price() {
-        Planet planet = createPlanet(Money.wons(5000),
-                Arrays.asList(Money.wons(1000), Money.wons(1000)),
-                Arrays.asList(Money.wons(1000), Money.wons(1000)));
+        Planet planet = createPlanet(wons(5000),
+                Arrays.asList(wons(1000), wons(1000)),
+                Arrays.asList(wons(1000), wons(1000)));
 
-        PriceSpecification specification = new PriceSpecification(Money.wons(9000));
+        PriceSpecification specification = new PriceSpecification(wons(9000));
 
         assertThat(specification.isSatisfiedBy(planet)).isTrue();
     }
+
+    @DisplayName("Test Data Builder 패턴으로 리팩터링하여 제조 가격 이외의 불필요한 정보를 빌더 내부에 숨길 수 있다")
+    @Test
+    void priceByTestDataBuilderPattern() {
+        Planet planet = aPlanet().with(anAtomsphere().with(wons(5000)))
+                        .with(aContinent().with(aNation().with(wons(1000)), aNation().with(wons(1000))))
+                        .with(anOcean().with(wons(1000)), anOcean().with(wons(1000)))
+                        .build();
+
+        PriceSpecification specification = new PriceSpecification(wons(9000));
+
+        assertThat(specification.isSatisfiedBy(planet)).isTrue();
+    }
+
 }
