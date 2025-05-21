ed145b3994ec99c07a47fb34a6d67564f34467b2
leejinwoo <leejinwoo1126@gmail.com>
2025-05-21 12:56:30 +0900

    refactor: combine object mother and test data builders

--- src/test/java/example2/objectmother/Customers.java
+++ src/test/java/example2/objectmother/Customers.java
@@ -2,11 +2,21 @@ package example2.objectmother;
 
 import example2.Address;
 import example2.Customer;
+import example2.builder.AddressBuilder;
+import example2.builder.CustomerBuilder;
 
 public class Customers {
     private Customers() {}
 
     public static Customer createCustomerWithAddress(Address address) {
         return new Customer(1L, "tester", address);
+    }
+
+    // Combine Builders and Object Mothers
+    public static CustomerBuilder aCustomer() {
+        return CustomerBuilder.aCustomer()
+                .withId(1L)
+                .withName("tester")
+                .withAddress(AddressBuilder.address().build());
     }
 }
--- src/test/java/example2/objectmother/Orders.java
+++ src/test/java/example2/objectmother/Orders.java
@@ -2,11 +2,21 @@ package example2.objectmother;
 
 import example2.Customer;
 import example2.Order;
+import example2.builder.CustomerBuilder;
+import example2.builder.OrderBuilder;
 
 public class Orders {
     private Orders() {}
 
     public static Order createOrderWithCustomer(Customer customer) {
         return new Order(1L, customer,0.0, null);
+    }
+
+    // Combine Builder Pattern and Object Mother Pattern
+    public static OrderBuilder anOrder() {
+        return OrderBuilder.aOrder()
+                .with(1L)
+                .with(CustomerBuilder.aCustomer().build())
+                .with(0.0);
     }
 }
